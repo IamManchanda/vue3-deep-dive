aa750435281cac3d199ccb90c213c7c8e7774a14
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-21 19:24:54 +0530

    for loop practise for render fn's

--- src/components/render-fn-practice/2.js
+++ src/components/render-fn-practice/2.js
@@ -3,6 +3,14 @@ import { h } from "vue";
 export default {
   name: "RenderFn2",
   render() {
-    return this.li;
+    return this.list.map(item => {
+      return h(
+        "div",
+        {
+          key: item.id,
+        },
+        item.text,
+      );
+    });
   },
 };
